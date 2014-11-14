bda12fb4aaac6c8b48dcb504d0ecb0368d89bf18
johnxpan <johnxpan@users.noreply.github.com>
2014-11-13 22:54:59 -0800

    Update shellsort.c

--- C/shellsort.c
+++ C/shellsort.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 void shellSort(int v[], int n) { //n is the len of the string
-    int gap, i, j, temp;
+    int gap, i, j;
     for(gap=n/2; gap> 0; gap /= 2) { //start with n/2, then n/4, n/8… till 1;
       for(i=gap; i<n;  i++) { // step along elements
          for(j=i-gap; j>=0 && v[j]>v[j+gap]; j-=gap) {
-            temp=v[j];
+            int temp=v[j];
             v[j]=v[j+gap];
             v[j+gap] = temp;
         }
