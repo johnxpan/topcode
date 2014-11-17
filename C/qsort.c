5024c88a02056e5bf6defaf5816b55272ac46be7
John <johnxpan@gmail.com>
2014-11-17 09:16:16 -0800

    Add new implementation from style book

--- C/qsort.c
+++ C/qsort.c
@@ -11,13 +11,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// interchange v[i], v[j]
 void swap(int v[], int i, int j) {
      int temp = v[i];
      v[i] = v[j];
      v[j] = temp;
 }
 
-void qsort(int v[], int left, int right) {
+void quicksort(int v[], int n) {
+   int i, last;
+   if(n<=1) // nothing to do, base case
+      return;
+   swap(v, 0, rand()%n);
+   last = 0;  // last is the beginning for smaller value
+   for(i=1; i<n; i++) //partition
+      if(v[i]<v[0])
+         swap(v, ++last, i); //swap the small value to the front
+   swap(v, last, 0);  //restore pivot
+   // now element from 1 to last are smaller than pivot
+   // element last to n-1 are larger than pivot
+   quicksort(v, last); //recursively sort 
+   quicksort(v+last+1, n-last-1); // each partition
+}
+
+void mqsort(int v[], int left, int right) {
      int pivot;
      int lo, hi;
 
@@ -38,13 +55,11 @@ void qsort(int v[], int left, int right) {
      } 
      swap(v, left, hi);
 
-     qsort(v, left, hi -1);
-     qsort(v, hi +1, right);
+     mqsort(v, left, hi -1);
+     mqsort(v, hi +1, right);
 }   
 
-
-void printArray(int a[], int n)
-{
+void printArray(int a[], int n) {
     int i=0;
     for(i=0; i< n; i++) {
        printf("%d, ", a[i]);
@@ -52,10 +67,8 @@ void printArray(int a[], int n)
 }
 
 int main(void) {
-  int v[10] = {11, 4, 8, 13, 57, 6, 18, 19, 22, 1};
-  qsort (v, 0, 9);
+  int v[10] = {11, 4, 8, 13, 57, 6, 18, 19, 22};
+  quicksort (v, 9);
   printArray(v, 9);
   return 0;
 }
-~                                                                                                                  
-~             
