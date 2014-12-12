9b7f30c90e6ffa33971315c4a8f4a73f2d28b8f0
johnxpan <johnxpan@users.noreply.github.com>
2014-12-12 14:09:30 -0800

    Update HashMapDemo.java

--- java/HashMapDemo.java
+++ java/HashMapDemo.java
@@ -1,5 +1,11 @@
 import java.util.*;
-
+//HashMap and Hashtable are almost identical
+//Hashtable is synchronized, whereas HashMap is not. This makes HashMap better for non-threaded applications
+// as unsynchronized Objects typically perform better than synchronized ones.
+// Hashtable does not allow null keys or values.  HashMap allows one null key and any number of null values.
+//One of HashMap's subclasses is LinkedHashMap, so in the event that you'd want predictable iteration order
+//(which is insertion order by default), you could easily swap out the HashMap for a LinkedHashMap. 
+//This wouldn't be as easy if you were using Hashtable.
 public class HashMapDemo {
    public static void main(String args[]) {
       // Create a hash map
